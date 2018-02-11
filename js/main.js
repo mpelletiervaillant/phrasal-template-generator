e2e2d43dbc4503f272a0809aeb42335f5de88895
mpelletiervaillant <mpelletiervaillant@Melanys-MacBook-Pro.local>
2018-02-11 13:18:26 -0500

    Set variables for story

---
+++ js/main.js
@@ -0,0 +1,9 @@
+alert("Let's make a story together!");
+
+var color = prompt("A colour (orange, indigo, etc)");
+
+var creature = prompt("A creature (dragon, moth, etc)");
+
+var adjective = prompt("A adjective (beautiful, super, etc)");
+
+var verb = prompt("A past tense verb (ran, burped, etc)");
