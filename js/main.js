3b88fa9d4da32ec5aee0cfe3753a7235869e9c89
mpelletiervaillant <mpelletiervaillant@Melanys-MacBook-Pro.local>
2018-02-11 17:40:12 -0500

    Wrote all the code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-alert("Let's make a story together!");
+var colour = '';
+var creature = '';
+var adjective = '';
+var verbed = '';
+var story = '';
 
-var color = prompt("A colour (orange, indigo, etc)");
+alert('Let’s make a story together!');
 
-var creature = prompt("A creature (dragon, moth, etc)");
+colour = prompt('A colour (orange, indigo, etc)');
 
-var adjective = prompt("A adjective (beautiful, super, etc)");
+creature = prompt('A creature (dragon, moth, etc)');
 
-var verb = prompt("A past tense verb (ran, burped, etc)");
+adjective = prompt('An adjective (beautiful, super, etc)');
+
+verbed = prompt('A past tense verb (ran, burped, etc)');
+
+story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning \nEnter the letter below:');
+
+if (story == 'a') {
+  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
+} if (story == 'b') {
+  document.write('Amelia ' + verbed + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + '.');
+} if (story == 'c') {
+  document.write('Jackson chose his ' + adjective + ' ' + colour + ' ' + creature + ' card and ' + verbed + ' it to the table knowing he won.');
+}
